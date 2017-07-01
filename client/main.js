8fb35db8cba46fd8ad11f25e32db46a8c31f3ad7
AD1121 <sinagh30@gmail.com>
2017-07-01 11:45:11 +0200

    Remove Button

--- client/main.js
+++ client/main.js
@@ -1,18 +1,28 @@
-
 // Load all the images from database
- Template.pictures.helpers({
-   images: Images.find({}, {
-      sort: { uploadedAt: -1 }
-    })
- });
+Template.pictures.helpers({
+  images: Images.find({}, {
+    sort: {
+      uploadedAt: -1
+    }
+  })
+});
 
 /* Inserting a image in the database */
- Template.main.events({
-   'change #myFileInput': function(event, template) {
-     event.preventDefault();
-     FS.Utility.eachFile(event, function(file) {
-       //Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
-       Images.insert(file, function(err, fileObj) {});
-     });
-   }
- });
+Template.main.events({
+  'change #myFileInput': function(event, template) {
+    event.preventDefault();
+    FS.Utility.eachFile(event, function(file) {
+      //Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+      Images.insert(file, function(err, fileObj) {});
+    });
+  }
+});
+
+/* Remove a image */
+Template.pictures.events({
+  'click .halfway-fab': function(e) {
+    Images.remove({
+      _id: this._id
+    });
+  }
+});
